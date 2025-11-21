94ae98caeb60168828830c48f824e70697934c85
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-11-21 12:43:27 +0000

    modified:   acc/.gitignore 	modified:   acc/pages/_app.tsx

--- acc/pages/_app.tsx
+++ acc/pages/_app.tsx
@@ -1,3 +1,4 @@
+//DATABASE_URL="mongodb+srv://waleedsheeraz06_db_user:rZ5aXdGJesBd5kPN@exp.wzyamyu.mongodb.net/acc?retryWrites=true&w=majority"
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
