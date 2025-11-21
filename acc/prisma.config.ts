e0ec48bded23d29ad1dc625f0399d88e26648b02
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-11-21 12:40:04 +0000

    modified:   acc/prisma.config.ts 	deleted:    acc/prisma/prisma.config.ts

--- acc/prisma.config.ts
+++ acc/prisma.config.ts
@@ -1,9 +1,12 @@
-// prisma.config.ts
 import { defineConfig } from "@prisma/config";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing DATABASE_URL in environment variables");
+}
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL, // MongoDB URL here
+    url: process.env.DATABASE_URL, // TypeScript now knows this is a string
   },
 });
