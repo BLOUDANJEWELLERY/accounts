07b9b7dff3e4f0a268a0970404e9cbb6ddf52f74
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-11-21 13:01:30 +0000

    modified:   acc/package.json 	deleted:    acc/prisma.config.ts 	modified:   acc/prisma/schema.prisma 	modified:   acc/server/prisma.ts

--- acc/server/prisma.ts
+++ acc/server/prisma.ts
@@ -1,21 +1,14 @@
 // server/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("Missing DATABASE_URL");
-}
-
 declare global {
   var prismaGlobal: PrismaClient | undefined;
 }
 
-// @ts-ignore: bug in Prisma v7 typings
-export const prisma = global.prismaGlobal ?? new PrismaClient({
-  log: ["query"],
-  datasourceUrl: databaseUrl,
-});
+export const prisma =
+  global.prismaGlobal ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
-if (process.env.NODE_ENV !== "production") {
-  global.prismaGlobal = prisma;
-}
+if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
