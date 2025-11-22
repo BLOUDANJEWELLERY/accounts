4591df4a1fb9bf929cbbe2ca5590eab32fe81529
WALEED SHEERAZ <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-11-22 11:37:38 +0300

    Update [accountNo].ts

--- acc/pages/api/customers/account/[accountNo].ts
+++ acc/pages/api/customers/account/[accountNo].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
+import { prisma } from "../../../../server/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { accountNo } = req.query;
